4c3c4031a022f5754dbdfa0c3dfbb4030abc21a0
bimales <mail2bimales@gmail.com>
2023-06-23 03:46:06 +0530

    Initial commit

---
+++ src/main/java/org/misson/impossible/misson/impossible/MissonImpossibleApplication.java
@@ -0,0 +1,13 @@
+package org.misson.impossible.misson.impossible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissonImpossibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissonImpossibleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/misson/impossible/misson/impossible/MissonImpossibleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.misson.impossible.misson.impossible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissonImpossibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
